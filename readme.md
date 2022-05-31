8854afac573eb6b7e15d8da07727c0e94cbfcaea
Leonardo Galante <lndgalante@gmail.com>
2022-05-31 17:44:03 -0300

    📦 NEW: Move logic to src folder

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## TODO
 
-- [ ] Logo para el menu contextual
-- [ ] Creo que no es necesario TS, pero abierto a discutirlo
-- [ ] Testing, agregaría, pero habría que investigar como testear extensiones
+- [x] Logo para el menu contextual
+- [x] Creo que no es necesario TS, pero abierto a discutirlo
+- [x] Testing, agregaría, pero habría que investigar como testear extensiones
 - [ ] Agregar botón en una esquina inferior de [Buscala.TV](https://Buscala.tv) que te lleve al Chrome Web Store
--- src/background.js
+++ src/background.js
@@ -1,10 +1,10 @@
-// helpers
+// ⚒️ helpers
 function getBuscalaTvUrl(movie) {
   const queryParam = encodeURIComponent(movie);
   return `https://buscala.tv/?search=${queryParam}`;
 }
 
-// attach "buscalaMenuContext" menu context to all pages
+// 📎 attach "buscalaMenuContext" menu context to all pages
 function handleInstallContextMenu() {
   chrome.contextMenus.create({
     contexts: ['selection'],
@@ -15,7 +15,7 @@ function handleInstallContextMenu() {
 
 chrome.runtime.onInstalled.addListener(handleInstallContextMenu);
 
-// attach click event for "buscalaMenuContext" menu context
+// 🖱 attach click event for "buscalaMenuContext" menu context
 function handleContextMenuClick({ selectionText }) {
   const url = getBuscalaTvUrl(selectionText);
   chrome.tabs.create({ url });
